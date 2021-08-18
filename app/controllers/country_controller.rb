2e82d42dd1f3ef3a176ff67bb26a32938271f017
Jon Tolentino <jontolentinocorp@gmail.com>
2021-08-18 20:42:04 +0800

    created country_controller.rb and started methods

---
+++ app/controllers/country_controller.rb
@@ -0,0 +1,24 @@
+class CountryController < ApplicationController
+
+    def show
+    end
+
+    def starts_with
+    end
+
+    def capital
+    end
+
+    def location
+    end
+
+    def money
+    end
+
+    def flag
+    end
+
+    def nums
+    end
+
+end
