f567a682cda49791da8c25c5f4360e9ba5c2af9f
Jon Tolentino <jontolentinocorp@gmail.com>
2021-08-18 20:32:50 +0800

    initial commit added routes

---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+  # name,population and demonym
+  get '/:name' => 'country#show', as: 'country_show'
+
+  # countries that start with that letter
+  get '/start/:name' => 'country#starts_with', as: 'country_starts_with'
+
+  # capital of the country
+  get ':name/capital' => 'country#capital', as: 'country_capital'
+
+  # ocation of the country
+  get ':name/loc' => 'country#location', as: 'country_location'
+
+  # data of the currencies of the country
+  get ':name/money' => 'country#money', as: 'country_money'
+
+  # flag of the country
+  get ':name/flag' => 'country#flag', as: 'country_flag'
+
+  # countries with this population
+  get 'pop/:nums' => 'country#nums', as: 'country_nums'
+
+end
