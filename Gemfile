6e53212fc293fdb0170f7900562998b0ece0bc0d
Jon Tolentino <jontolentino123@gmail.com>
2021-08-20 20:47:45 +0800

    change to ruby 3.0.1 added api endpoint for country_show

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '3.0.2'
+ruby '3.0.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.4'
@@ -27,6 +27,9 @@ gem 'jbuilder', '~> 2.7'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
+# faraday gem
+gem 'faraday'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ app/apis/countries/v1/client.rb
@@ -0,0 +1,61 @@
+module Countries
+    module V1
+        class Client
+            # include HttpStatusCodes
+            # include ApiExceptions
+ 
+            API_ENDPOINT = 'https://restcountries.eu/rest/v2'
+            
+ 
+            def initialize(oauth_token = nil)
+                @oauth_token = oauth_token
+            end
+ 
+            def country_show(country_name)
+                request(
+                    http_method: :get,
+                    endpoint: "name/#{country_name}/")
+            end
+ 
+            # def my_repo
+            #     request(
+            # http_method: :get,
+            # endpoint: "user"
+            # )
+            # end
+ 
+            private
+ 
+            def request(http_method:, endpoint:, params: {})
+                @response = connection.public_send(http_method, endpoint, params)
+                parsed_response = JSON.parse(@response.body)
+ 
+                return parsed_response if response_successful?
+ 
+                raise error_class, "Code: #{@response.status}, response: #{@response.body}"
+            end
+ 
+            def connection
+                @connection ||= Faraday.new(API_ENDPOINT) do |f|
+                    f.request :url_encoded
+                    f.adapter Faraday.default_adapter
+                end
+            end
+ 
+            def response_successful?
+                @response.status == HTTP_OK_CODE
+            end
+ 
+            def error_class
+                case @response.status
+                when HTTP_NOT_FOUND_CODE
+                    NotFoundError
+                when HTTP_UNPROCESSABLE_ENTITY_CODE
+          UnprocessableEntityError
+        else
+            ApiError
+        end
+            end
+        end
+    end
+end
