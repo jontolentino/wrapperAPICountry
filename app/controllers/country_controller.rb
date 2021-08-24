f18f4acef123b1100cac801602b2a02524308f3d
Jon Tolentino <jontolentino123@gmail.com>
2021-08-24 19:39:06 +0800

    json output for jsonrequest

--- app/controllers/country_controller.rb
+++ app/controllers/country_controller.rb
@@ -6,40 +6,71 @@ class CountryController < ApplicationController
 
         respond_to do |format|
             format.html { @c }
-            format.json { render json: json_format(@c) }
+            format.json { render json: @c }
         end
     end
 
     def starts_with
         country = Countries::V1::Client.new()
         @c = @country.country_starts_with(params[:letter])
+
+        respond_to do |format|
+            format.html { @c }
+            format.json { render json: @c }
+        end
     end
 
     def capital
         @c = @country.country_capital(params[:name])
+
+        respond_to do |format|
+            format.html { @c }
+            format.json { render json: @c }
+        end
     end
 
     def location
         @c = @country.country_location(params[:name])
         @loc = @c.to_json
+
+        respond_to do |format|
+            format.html { @loc }
+            format.json { render json: @loc }
+        end
     end
 
     def money
         @c = @country.country_money(params[:name])
+
+        respond_to do |format|
+            format.html { @c }
+            format.json { render json: @c }
+        end
     end
 
     def flag
         @c = @country.country_flag(params[:name])
+
+        respond_to do |format|
+            format.html { @c }
+            format.json { render json: @c }
+        end
     end
 
     def nums
         @c = @country.country_nums_less(params[:nums].to_i)
+
+        respond_to do |format|
+            format.html { @c }
+            format.json { render json: @c }
+        end
     end
 
     private
 
     def country_new
         @country = Countries::V1::Client.new()
+
     end
 
 end
